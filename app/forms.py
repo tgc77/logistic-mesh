fddbfce0304b5e74ecb1768f63f41f2a1af0e07b
James <tikx.batera@gmail.com>
2021-03-31 09:50:34 -0300

    Adding database settings and forms

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,13 @@ class UploadMapForm(FlaskForm):
     mapname = StringField('Mapname', validators=[DataRequired()])
     routes = TextAreaField('Routes', validators=[DataRequired()])
     submit = SubmitField('Upload')
+
+
+class FindBestRouteForm(FlaskForm):
+    mapname = StringField('Mapname', validators=[DataRequired()])
+    origin = StringField('Origin', validators=[DataRequired()])
+    destiny = StringField('Destiny', validators=[DataRequired()])
+    truck_autonomy = StringField('TruckAutonomy', validators=[DataRequired()])
+    liter_fuel_value = StringField(
+        'LiterFuelValue', validators=[DataRequired()])
+    submit = SubmitField('Find')
--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, flash, redirect, url_for
-from app import app
-from app.forms import UploadMapForm
+import json
+from app import app, db
+from app.forms import UploadMapForm, FindBestRouteForm
+from app.models import LogisticMeshMap
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html.j2')
+    return redirect(url_for('list_maps'))
+    # return render_template('index.html.j2')
 
 
 """
@@ -23,6 +26,33 @@ D E 30
 def upload_map():
     form = UploadMapForm()
     if form.validate_on_submit():
+        logistic_map = LogisticMeshMap()
+        logistic_map.mapname = form.mapname.data
+        log_map = [s.rstrip().split() for s in form.routes.data.split('\n')]
+        logistic_map.routes = json.dumps(log_map)
+        db.session.add(logistic_map)
+        db.session.commit()
         flash('Map uploaded successfully!')
         return redirect(url_for('index'))
     return render_template('upload_map.html.j2', title='Upload Map', form=form)
+
+
+@app.route('/list_maps')
+def list_maps():
+    logistic_maps = LogisticMeshMap.query.all()
+
+    return render_template('list_maps.html.j2', title='Map List', logistic_maps=logistic_maps)
+
+
+@app.route('/find_best_route')
+def find_best_route():
+    form = FindBestRouteForm()
+    if form.validate_on_submit():
+        logistic_map = LogisticMeshMap.query.filter_by(
+            mapname=form.mapname).first_or_404()
+
+    return render_template('show_best_route.html.j2', title='Best Route Result')
+
+
+def process_best_route_request(routes):
+    ...
