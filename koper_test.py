702e326659ba3132241ea3d563836125ce91ba16
James <tikx.batera@gmail.com>
2021-03-31 09:47:25 -0300

    Adding error handler features

--- koper_test.py
+++ koper_test.py
@@ -1,1 +1,12 @@
-from app import app
+from app import app, db
+from app.models import LogisticMeshMap
+
+
+@app.shell_context_processor
+def make_shell_context():
+    """Function to enable testing from python shell interface
+
+    Returns:
+        [db, LogisticMeshMap]: Database object and model
+    """
+    return {'db': db, 'LogisticMeshMap': LogisticMeshMap}
